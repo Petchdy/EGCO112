57c0b8bd2b37d393bc6974ee8c0d4070dbb22060
Kulwisit Sakkittiphokhin (Petch) <63772613+Petchdy@users.noreply.github.com>
2024-01-18 14:45:30 +0000

    add insertion_sort

---
+++ sorting/insertion_sort.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+void swap(int * a, int * b);
+void insertion_sort(int arr[], int lenght);
+void print_array(int arr[], int lenght);
+
+int main() {
+
+    int arr[] = {6, 5, 0, 1, 3, 2, 4, 8, 7};
+    int size = sizeof(arr)/sizeof(int);
+
+    insertion_sort(arr, size);
+
+    return 0;
+}
+
+void swap(int * a, int * b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertion_sort(int arr[], int lenght) {
+
+    printf("arr => ");
+    print_array(arr, lenght);
+    printf("\n");
+    printf("size = %d\n", lenght);
+
+    int temp;
+
+    for (int i = 0; i < lenght; i++) {
+
+        for (int j = i+1; j > 0 && arr[j] < arr[j-1]; j--) {
+            swap(&arr[j], &arr[j-1]);
+        }
+
+        print_array(arr, lenght);
+        printf("\n");
+    
+    }
+
+    // for (int i = 1; i < lenght; i++) {
+    
+    //     temp = arr[i];
+
+    //     for (int j = i-1; j >= 0 && arr[j] > temp ; j--) {
+    //         arr[j+1] = arr[j];
+    //         print_array(arr, lenght);
+    //         printf("\n");
+    //     }
+    //     arr[i-1] = temp;
+    //     print_array(arr, lenght);
+    //     printf("\n");
+    // }
+}
+
+void print_array(int arr[], int lenght) {
+    for (int i = 0; i < lenght; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+}
