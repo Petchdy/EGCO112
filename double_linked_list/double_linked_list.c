8ab3b52f37298113cc987fdce01d8184d571a00d
Kulwisit Sakkittiphokhin (Petch) <63772613+Petchdy@users.noreply.github.com>
2024-02-20 16:47:34 +0000

    starting dbll

---
+++ double_linked_list/double_linked_list.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"double_ll.h"
+
+int main () {
+
+}
---
+++ double_linked_list/double_ll.h
@@ -0,0 +1,7 @@
+struct double_ll {
+    int value;
+    struct double_ll * next;
+    struct double_ll * prev;
+};
+typedef stuct double_ll dll;
+
